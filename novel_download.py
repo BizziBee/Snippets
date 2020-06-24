c2426973c725d59a6afb7fc61c9711cf0cf09aff
ormond <ormond@email.com>
2020-06-24 19:58:39 +1000

    从页面提取文本雏形

--- novel_download.py
+++ novel_download.py
@@ -1,9 +1,16 @@
 #
 
 import requests
+from pyquery import PyQuery as pq
 
 response = requests.get('http://www.jianlaixiaoshuo.com/book/1.html')
 response.encoding = response.apparent_encoding
+# print(response.text)
 
-print(response.text)
+doc = pq(response.text)
+
+title = doc('#BookCon > h1').text()
+content = doc('#BookText').text()
+
+print(content)
 
