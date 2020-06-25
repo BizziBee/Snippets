c285a7a935b069e9afc25008812bb50cd43e75a8
ormond <ormond@email.com>
2020-06-25 14:51:17 +1000

    从网站自动下载小说至<剑来.txt>

--- novel_download.py
+++ novel_download.py
@@ -1,16 +1,29 @@
 #
+#
 
 import requests
 from pyquery import PyQuery as pq
 
-response = requests.get('http://www.jianlaixiaoshuo.com/book/1.html')
+#  从小说目录页 http://www.jianlaixiaoshuo.com/ 获取所有页面链接
+response = requests.get('http://www.jianlaixiaoshuo.com/')
 response.encoding = response.apparent_encoding
-# print(response.text)
-
 doc = pq(response.text)
 
-title = doc('#BookCon > h1').text()
-content = doc('#BookText').text()
+links = doc('dl > dd a')
+for link in links.items():
+    # print("http://www.jianlaixiaoshuo.com" + link.attr('href'))
+    response = requests.get("http://www.jianlaixiaoshuo.com" + link.attr('href'))
+    response.encoding = response.apparent_encoding
+    doc = pq(response.text)
+    title = doc('#BookCon > h1').text()
+    content = doc('#BookText').text()
 
-print(content)
+    # 使用上下文管理器打开文件
+    with open('剑来.txt', mode='a+', encoding='utf-8') as f:
+        f.write(title)
+        f.write(content)
+        f.write('/n')
+
+
+
 
